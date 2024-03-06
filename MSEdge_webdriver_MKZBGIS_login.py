89f728a93bf83830bfd51b9f176b75aabebb1b37
martin.kelko <martin2kelko@gmail.com>
2024-03-06 21:12:14 +0100

    feat: login to the MKZBGIS portal

--- MSEdge_webdriver_MKZBGIS_login.py
+++ MSEdge_webdriver_MKZBGIS_login.py
@@ -15,26 +15,26 @@ driver.get(url)
 sleep(8)
 
 # Click on "window"
-window_selector = "body > app-root > app-content-layout > mat-drawer-container > mat-drawer-content > mat-drawer-container > mat-drawer > div > app-login > div > form > ng-scrollbar > div > div > div > div > div > div.actions > button > span.mat-mdc-button-touch-target"
-window = driver.find_element(By.CSS_SELECTOR, window_selector)
+window_selector = "/html/body/div[2]/div[2]/div/mat-dialog-container/div/div/app-about-modal/mat-dialog-actions/button/span[2]"
+window = driver.find_element(By.XPATH, window_selector)
 window.click()
 sleep(2)
 
 # Email input field
-email_field_selector = "#mat-mdc-form-field-label-2 > mat-label"
-email_field = driver.find_element(By.CSS_SELECTOR, email_field_selector)
+email_field_selector = "/html/body/app-root/app-content-layout/mat-drawer-container/mat-drawer-content/mat-drawer-container/mat-drawer/div/app-login/div/form/ng-scrollbar/div/div/div/div/div/mat-form-field[1]/div[1]/div/div[2]/input"
+email_field = driver.find_element(By.XPATH, email_field_selector)
 email_field.send_keys(username)
 sleep(3)
 
 # Password input field
-password_field_selector = "#mat-input-2"
-password_field = driver.find_element(By.CSS_SELECTOR, password_field_selector)
+password_field_selector = "/html/body/app-root/app-content-layout/mat-drawer-container/mat-drawer-content/mat-drawer-container/mat-drawer/div/app-login/div/form/ng-scrollbar/div/div/div/div/div/mat-form-field[2]/div[1]/div/div[2]/input"
+password_field = driver.find_element(By.XPATH, password_field_selector)
 password_field.send_keys(password)
 sleep(3)
 
 # Click on "Prihlasit"
-prihlasit_button_selector = "button.mdc-button:nth-child(2) > span:nth-child(2)"
-prihlasit_button = driver.find_element(By.CSS_SELECTOR, prihlasit_button_selector)
+prihlasit_button_selector = "/html/body/app-root/app-content-layout/mat-drawer-container/mat-drawer-content/mat-drawer-container/mat-drawer/div/app-login/div/form/ng-scrollbar/div/div/div/div/div/div[1]/button/span[2]"
+prihlasit_button = driver.find_element(By.XPATH, prihlasit_button_selector)
 prihlasit_button.click()
 sleep(3)
 
